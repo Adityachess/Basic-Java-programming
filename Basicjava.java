3955bbcf9080a40faeed06c42f04f2ad8ae59421
Adityachess <adityadone9398@gmail.com>
2021-09-22 14:22:44 +0530

    Checking whether year entered by the user is a leapyear or not

--- Basicjava.java
+++ Basicjava.java
@@ -6,46 +6,21 @@ public class Basicjava {
 
 	public static void main(String[] args) {
 
-		int Heads = 0;
-		int Tails = 0;
-		int Counter = 1;
+		int year;
 
-		double randNum = 0.0;
+		Scanner sc = new Scanner(System.in);
 
-		Scanner Input = new Scanner(System.in);
+		System.out.println("Enter any year of your choice: ");
 
-		System.out.println("The numbres of times to filp coin ");
+		year = sc.nextInt();
 
-		double flips = Input.nextDouble();
-
-		while (Counter <= flips) {
-
-			randNum = Math.random();
-
-			System.out.print(Counter + " " + randNum);
-
-			if (randNum < 0.5) {
-				Heads++;
-				System.out.println(" heads ");
-			} 
-			else {
-				Tails++;
-				System.out.println(" tails ");
-			}
-			Counter++;
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			System.out.println("this is leap year");
 		}
 
-		System.out.println();
-		System.out.println("No. of heads equal to :" + Heads);
-		System.out.println("No. of tails equal to :" + Tails);
-
-		double percentage_heads = ((Heads / flips) * 100);
-		double percentage_tails = ((Tails / flips) * 100);
-
-		System.out.println("The percentage of heads is :" + percentage_heads);
-
-		System.out.println("The percentage of heads is :" + percentage_tails);
-
+		else {
+			System.out.println("this is not leap year");
+		}
 	}
 
 }
