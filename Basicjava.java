b894038cac15db1c6e9cdca3b566da0ebf994341
Adityachess <adityadone9398@gmail.com>
2021-09-22 14:13:31 +0530

    welcome Message

---
+++ Basicjava.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.basicjava;
+
+public class Basicjava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Basic Core Java Programming");
+
+	}
+
+}
