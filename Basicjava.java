f8188f574d263f196a1a9ccec304cd84f9da80b6
Adityachess <adityadone9398@gmail.com>
2021-09-22 14:47:25 +0530

    Swapping the two numbers

--- Basicjava.java
+++ Basicjava.java
@@ -6,15 +6,18 @@ public class Basicjava {
 
 	public static void main(String[] args) {
 
-		double dividend = 43;
-		double divisor = 5;
-
-		double quotient = dividend / divisor;
-		double remainder = dividend % divisor;
-
-		System.out.println("Quotient is equal :" + quotient);
-
-		System.out.println("Remainder is equal :" + remainder);
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers to Swap");
+		System.out.println("Enter Number 1");
+		int num1 = sc.nextInt();
+		System.out.println("Enter Number 2");
+		int num2 = sc.nextInt();
+			
+		int swap = num1;
+		num1 = num2;
+		num2 = swap;
+		System.out.println("After Swaping the number the final value is");
+		System.out.println("The first number 1 is :" +num1);
+		System.out.println("The Second number 2 is :" +num2);
 	}
 }
