74ca95b46616937439731a2106e8f2af3efa1747
Adityachess <adityadone9398@gmail.com>
2021-09-22 14:49:47 +0530

    Checking whether entered number is even or odd

--- Basicjava.java
+++ Basicjava.java
@@ -7,17 +7,16 @@ public class Basicjava {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter two numbers to Swap");
-		System.out.println("Enter Number 1");
-		int num1 = sc.nextInt();
-		System.out.println("Enter Number 2");
-		int num2 = sc.nextInt();
-			
-		int swap = num1;
-		num1 = num2;
-		num2 = swap;
-		System.out.println("After Swaping the number the final value is");
-		System.out.println("The first number 1 is :" +num1);
-		System.out.println("The Second number 2 is :" +num2);
+
+		System.out.println("Enter the num to Check Even or Odd :");
+
+		int num = sc.nextInt();
+
+		if (num % 2 == 0) {
+			System.out.println("The Entered number is even");
+		} 
+		else {
+			System.out.println("The Entered number is odd");
+		}
 	}
 }
